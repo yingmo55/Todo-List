93d49c039b6b6eebba644bdc4830898a9409b579
Ying <97061798+yingmo55@users.noreply.github.com>
2022-02-23 17:38:41 -0500

    Add basic function and state
    - Add state hooks for individual task and todoList
    - Add input field for adding items
    - Add button for adding more tasks

--- src/Components/todo/todo.js
+++ src/Components/todo/todo.js
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 function Todo() {
+const [todo, setTodo] = useState('');
+const [todoList, setTodoList] = useState([]);
+const todoItems = document.getElementById('todoItems');
+
+const updateTodoField = e => {
+    e.preventDefault();
+    setTodo(e.target.value);
+}
+const addToTodos = () => {
+    setTodoList((prev) =>
+        [...prev, {
+            task: todo,
+            time: '1970-1-1'
+            }
+        ])
+    setTodo('')
+    todoItems.value = '';
+}
+
+
+const renderList = () => {
+    const listItem = todoList.map(todo, index =>
+        <li key={'key'+index}>{todo.task}</li>
+        )
+    return listItem;
+}
 
     return (
         <>
+            <input id='todoItems' type='text' onChange={updateTodoField} />
+            <button onClick={addToTodos}>+</button>
         <ol>
-            <li>test</li>
+            <hr/>
         </ol>
         </>
     )
 }
+
 
 export default Todo;
