b0e946306292da65c67c167fc0ca1e386f9e195e
Ying <97061798+yingmo55@users.noreply.github.com>
2022-02-24 00:05:25 -0500

    Add function to pause timer

--- src/Components/Pomodoro/Timer/Timer.js
+++ src/Components/Pomodoro/Timer/Timer.js
@@ -2,22 +2,38 @@ import React, {useState, useEffect} from "react";
 
 function Timer () {
 const [time, setTime] = useState(5000);
+const [pauseTimer, setPauseTimer] = useState(false);
+
+const toggleTimerButton = document.getElementById('toggleTimer');
 
 const countdown = () => {
     setTime((prev)=> prev - 1);
 }
 
 useEffect(() => {
-    const timeInterval = setInterval(countdown, 1000);
+    if (!pauseTimer) {
+        const timeInterval = setInterval(countdown, 1000);
     return () => {
         clearInterval(timeInterval);
     };
-}, [time])
+}
+}, [time, pauseTimer])
+
+const toggleTimer = () => {
+    setPauseTimer(!pauseTimer);
+    toggleTimerButton.innerHTML = pauseTimer ?  'Currently: on' : 'Currently: off';
+}
 
 
     return (
         <>
         <p>{time}</p>
+        <input type="text" name="timer" list="time" />
+        <datalist id="time">
+            <option value="5" />
+            <option value="10" />
+        </datalist>
+        <button onClick={toggleTimer} id='toggleTimer'>Pause</button>
         </>
     );
 }
