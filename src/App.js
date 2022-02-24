5635aae49f203571168e24a8fad0bf65b99e4453
Ying <97061798+yingmo55@users.noreply.github.com>
2022-02-23 22:39:54 -0500

    Move todo list components to TodoList/

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Todo from './Components/TodoList/Todo/Todo';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+      <Todo />
       </header>
     </div>
   );
