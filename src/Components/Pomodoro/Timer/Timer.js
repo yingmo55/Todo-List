eaccd025c6e9e416e170d9bc2d5db4be59ec238a
Ying <97061798+yingmo55@users.noreply.github.com>
2022-02-24 15:54:07 -0500

    Add function to change timer
    - Add state hook for starting timer
    - Add selecte function to change timer

--- src/Components/Pomodoro/Timer/Timer.js
+++ src/Components/Pomodoro/Timer/Timer.js
@@ -1,9 +1,11 @@
 import React, {useState, useEffect} from "react";
 
 function Timer () {
-const [time, setTime] = useState(100);
-const [pauseTimer, setPauseTimer] = useState(false);
+const [time, setTime] = useState(5);
+const [pauseTimer, setPauseTimer] = useState(true);
 const toggleTimerButton = document.getElementById('toggleTimer');
+const [timerStart, setTimerStart] = useState(false);
+const setTimerSelect = document.getElementById('setTimerSelect');
 
 const countdown = () => {
     setTime((prev)=> prev - 1);
@@ -11,40 +13,70 @@ const countdown = () => {
 
 const toggleTimer = () => {
     setPauseTimer(!pauseTimer);
+    if (time === 0) {
+        console.log('times up. Take a break!')
+    }
 }
 
 const parseTime = () => {
-    let min = Math.floor(time / 60);
-    let sec = time % 60;
-    return `${min}: ${sec}`;
+    let min = Math.floor(time / 60) >= 10 ? Math.floor(time / 60) : "0" + Math.floor(time / 60);
+    let sec = time % 60 >= 10 ? time % 60 : "0" + time % 60;
+    return `${min}:${sec}`;
 }
 
 const parsedTime = parseTime();
 
 useEffect(() => {
-    if (!pauseTimer) {
+    if (!pauseTimer && time > 0) {
         const timeInterval = setInterval(countdown, 1000);
-        
     return () => {
         clearInterval(timeInterval);
     };
 }
 }, [time, pauseTimer])
 
+useEffect(()=>{
+    if (time <= 0) {
+        console.log('times up. Take a break!')
+        setPauseTimer(true);
+        setTimerStart(false);
+    }
+}, [time])
+
 useEffect(()=> {
     if (toggleTimerButton){
         toggleTimerButton.innerHTML = pauseTimer ?  'Currently: off' : 'Currently: on';
-        }
+        } 
 }, [toggleTimerButton, pauseTimer])
+
+const startTimer = () => {
+    console.log('timer start!')
+    setPauseTimer(false);
+    setTimerStart(true);
+}
+
+const changeTimer = e => {
+    e.preventDefault();
+    setTime(e.target.value * 60);
+    setPauseTimer(true);
+}
 
     return (
         <>
+        <p>🍅</p>
         <p>{parsedTime}</p>
-        <input type="text" name="timer" list="time" />
-        <datalist id="time">
-            <option value="5" />
-            <option value="10" />
-        </datalist>
+        { !timerStart &&
+        (<div id='setTimerSelect'>
+        <label htmlFor='timer'>Set timer here:</label>
+        <select id="timer" list="time" onChange={changeTimer} >
+            <option  value="">---select a time---</option>
+            <option  value="0.1">test</option>
+            <option  value="5">5 min - great for a kickstart!</option>
+            <option value="10">10 min - for a short task</option>
+            <option value="25">25 min- standard pomodoro</option>
+        </select>
+        <button type='button' onClick={startTimer}>Start Timer</button>
+        </div>)}
         <button onClick={toggleTimer} id='toggleTimer'>Loading...</button>
         </>
     );
