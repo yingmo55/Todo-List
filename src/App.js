65c7b49ca5413125a0761e913c3a19a3ee5e4542
Ying <97061798+yingmo55@users.noreply.github.com>
2022-02-23 23:37:36 -0500

    Add basic countdown function
    - Add test countdown function in Timer component
    - Add display to countdown

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Todo from './Components/TodoList/Todo/Todo';
+import Timer from './Components/Pomodoro/Timer/Timer';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Timer />
       <Todo />
       </header>
     </div>
--- src/Components/Pomodoro/Timer/Timer.js
+++ src/Components/Pomodoro/Timer/Timer.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 function Timer () {
+const [time, setTime] = useState(5000);
 
+const countdown = () => {
+    setTime((prev)=> prev - 1);
+}
+
+useEffect(() => {
+    const timeInterval = setInterval(countdown, 1000);
+    return () => {
+        clearInterval(timeInterval);
+    };
+}, [time])
+
+
+    return (
+        <>
+        <p>{time}</p>
+        </>
+    );
 }
 
 export default Timer;
