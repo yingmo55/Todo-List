2e5baf3523ab59ce48abca4fbb7c8cf049ef4c24
Ying <97061798+yingmo55@users.noreply.github.com>
2022-02-24 20:26:37 -0500

    Modify add task functions
    - disallow adding empty tasks to the list
    - disallow adding task starting with an empty space
    - add warning message if user attempts to do the above actions

--- src/Components/TodoList/Todo/Todo.js
+++ src/Components/TodoList/Todo/Todo.js
@@ -3,21 +3,28 @@ import TaskList from '../TaskList/TaskList';
 
 function Todo() {
 const [todo, setTodo] = useState('');
+const [warning, setWarning] = useState(false);
 const [todoList, setTodoList] = useState([]);
 const todoItems = document.getElementById('todoItems');
 
 const updateTodoField = e => {
     e.preventDefault();
     setTodo(e.target.value);
+    setWarning(false);
 }
 
 const addToTodos = () => {
+    if (todo[0] === ' ' || !todo) {
+        setWarning(true);
+        return;
+    }
+    setWarning(false);
     setTodoList((prev) =>
-        [...prev, {
-            task: todo,
-            id: Date.now()
-            }
-        ])
+    [...prev, {
+        task: todo,
+        id: Date.now()
+        }
+    ])
     setTodo('');
     todoItems.value = '';
 }
@@ -35,6 +42,8 @@ const deleteItem = deletedItem => {
             <button onClick={addToTodos}>+</button>
             <hr/>
         <TaskList todoList={todoList} deleteItem={deleteItem}/>
+        {warning && <p id='emptyTaskWarning'>The first character of a task and the task cannot be empty</p>}
+        {}
         </>
     )
 }
