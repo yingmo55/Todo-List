16274546f18fab96c5f27f26193173f6c4000d2d
Ying <97061798+yingmo55@users.noreply.github.com>
2022-02-24 20:03:16 -0500

    Add current time to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Todo from './Components/TodoList/Todo/Todo';
 import Timer from './Components/Pomodoro/Timer/Timer';
+import CurrentTime from './Components/CurrentTime/CurrentTime';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <CurrentTime />
         <Timer />
       <Todo />
       </header>
